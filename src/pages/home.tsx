ab3c7fdc8277943b9fcd562673331fe6929aebfb
kainesv86 <kainesv86@gmail.com>
2021-05-29 02:27:42 +0700

    home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,11 +5,14 @@ export interface HomeProps {}
 
 const Home: React.FunctionComponent<HomeProps> = () => {
         return (
-                <div className="w-full mt-8">
+                <div className="flex-col items-center w-full mt-8">
                         <h1 className="text-3xl font-bold tracking-tight text-amber-500">The best place for Working</h1>
                         <h2 className="text-xl font-medium tracking-tight text-amber-500">
                                 Take advantage of team working by using Co-working Space today!
                         </h2>
+                        <div className="h-80 w-52">
+                                <img src="/share/home.svg" alt="" className="w-full h-full" />
+                        </div>
                 </div>
         );
 };

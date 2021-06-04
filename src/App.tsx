de861916eff58b2ef1bfd084f8b15a344ddc9415
kainesv86 <kainesv86@gmail.com>
2021-06-04 20:52:49 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 import Home from "./pages/home";
 import Locations from "./pages/locations/";
 import Solutions from "./pages/solutions/";
@@ -11,32 +12,35 @@ import { Route, Switch } from "react-router-dom";
 
 function App() {
         return (
-                <React.Fragment>
+                <div className="relative h-screen">
                         <NavBar />
-                        <Switch>
-                                <Route path="/locations">
-                                        <Locations />
-                                </Route>
-                                <Route path="/solutions">
-                                        <Solutions />
-                                </Route>
-                                <Route path="/gallery">
-                                        <ComingSoon />
-                                </Route>
-                                <Route path="/aboutus">
-                                        <ComingSoon />
-                                </Route>
-                                <Route path="/order/done">
-                                        <OrderDone />
-                                </Route>
-                                <Route path="/order">
-                                        <Order />
-                                </Route>
-                                <Route path="/">
-                                        <Home />
-                                </Route>
-                        </Switch>
-                </React.Fragment>
+                        <div className="h-auto">
+                                <Switch>
+                                        <Route path="/locations">
+                                                <Locations />
+                                        </Route>
+                                        <Route path="/solutions">
+                                                <Solutions />
+                                        </Route>
+                                        <Route path="/gallery">
+                                                <ComingSoon />
+                                        </Route>
+                                        <Route path="/aboutus">
+                                                <ComingSoon />
+                                        </Route>
+                                        <Route path="/order/done">
+                                                <OrderDone />
+                                        </Route>
+                                        <Route path="/order">
+                                                <Order />
+                                        </Route>
+                                        <Route path="/">
+                                                <Home />
+                                        </Route>
+                                </Switch>
+                        </div>
+                        <Footer />
+                </div>
         );
 }
 
---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Component } from "react";
+
+export interface FooterProps {}
+
+const Footer: React.FunctionComponent<FooterProps> = () => {
+        return <footer className="bottom-0 right-0 w-full py-16 mt-32 bg-amber-400"></footer>;
+};
+
+export default Footer;
